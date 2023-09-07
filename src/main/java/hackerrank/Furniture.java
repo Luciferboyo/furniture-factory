3ee412925e1917661d222d9f84977787d73a4d32
vincent.lau <vincentlau1006@gmail.com>
2023-09-07 15:40:57 +0800

    update

---
+++ src/main/java/hackerrank/Furniture.java
@@ -0,0 +1,36 @@
+package hackerrank;
+
+/**
+ * FURNITURE_TYPE("Furniture Name", floating-point cost)
+ */
+
+public enum Furniture {
+	CHAIR("Chair", 100.0f),
+	TABLE("Table", 250.0f),
+	COUCH("Couch", 500.0f);
+
+	private final String label;
+	private final float cost;
+
+	/**
+	 * @param label The plain text name of the furniture
+	 * @param cost  The furniture's cost
+	 */
+	// TODO: Create a constructor
+
+	/**
+	 * @return The plain text name of the furniture
+	 */
+	public String label() {
+    // TODO: Complete the method
+		return null;
+	}
+
+	/**
+	 * @return The furniture's cost
+	 */
+	public float cost() {
+    // TODO: Complete the method
+		return -1.0f;
+	}
+}
